3de980bfe30dbe4cec4745f394e04275e1644cc6
Prio Arief Gunawan <prio.gunawan@kanggo.id>
2025-06-27 13:55:50 +0700

    fix: handle not found post

--- src/v1/modules/post/post.controller.js
+++ src/v1/modules/post/post.controller.js
@@ -1,3 +1,4 @@
+const custom_error = require('../../helpers/custom_error');
 const http_response = require('../../helpers/http_response');
 const load_data = require('../../helpers/load_data');
 const PostValidation = require('./post.validation');
@@ -9,6 +10,10 @@ class PostController {
 
       const { communityId } = req.params;
       const posts = await load_data(+communityId);
+
+      if (posts.length < 1) {
+        throw custom_error(404, 'post not found')
+      }
 
       return http_response(res, 200, 'Success get posts', posts);
     } catch (error) {
